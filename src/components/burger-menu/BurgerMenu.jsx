228332967485e311ee5f62b9674fd0acc1a0c3d1
Eugen <eugenstel@gmail.com>
2022-02-22 19:53:02 +0300

    fix burger

--- src/components/burger-menu/BurgerMenu.jsx
+++ src/components/burger-menu/BurgerMenu.jsx
@@ -7,6 +7,7 @@ import './burger-menu.scss'
 export const BurgerMenu = ({ handleMobileClose, onClickOutside }) => {
     const ref = useRef(null);
 
+
     useEffect(() => {
         const handleClickOutside = (event) => {
             if (ref.current && !ref.current.contains(event.target)) {
@@ -14,17 +15,26 @@ export const BurgerMenu = ({ handleMobileClose, onClickOutside }) => {
                 document.body.style.overflow = 'visible'
             }
         };
+        const escFunction = (event) => {
+            if (event.keyCode === 27) {
+                onClickOutside && onClickOutside();
+                document.body.style.overflow = 'visible'
+            }
+        };
+
+        document.addEventListener("keydown", escFunction);
         document.addEventListener('click', handleClickOutside);
         return () => {
             document.removeEventListener('click', handleClickOutside);
+            document.removeEventListener("keydown", escFunction);
         };
     }, [onClickOutside]);
 
 
     return (
-        <div className='burger_menu' data-test-id='burger-menu' ref={ref}>
+        <div className='burger_menu' data-test-id='burger-menu' >
             {HEADER_MENU.map(({ id, path, name }) => (
-                <Link key={id} to={`/${path}`} className='menu_item' onClick={handleMobileClose}>
+                <Link key={id} to={`/${path}`} className='menu_item' onClick={handleMobileClose} ref={ref}>
                     <span>{name}</span>
                 </Link>
             ))}
